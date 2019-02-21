1892368c336fe0a6839124e6bbe670a211e5ca48
Kamil Halko <kamil@linkfactory.dk>
2019-02-21 08:02:22 +0100

    RemoteSearch changed to return empty list when requesting all songs

--- app/src/main/java/com/kamilh/findmysong/interactors/RemoteSearch.kt
+++ app/src/main/java/com/kamilh/findmysong/interactors/RemoteSearch.kt
@@ -3,14 +3,18 @@ package com.kamilh.findmysong.interactors
 import com.kamilh.findmysong.data.Song
 import com.kamilh.findmysong.repository.Resource
 import com.kamilh.findmysong.repository.remote.SearchRepository
+import com.kamilh.findmysong.views.search.Query
 import io.reactivex.Single
 import javax.inject.Inject
 
 class RemoteSearch @Inject constructor(
     private val searchRepository: SearchRepository
-) : SingleInteractor<String, Resource<List<Song>>> {
+) : SingleInteractor<Query, Resource<List<Song>>> {
 
-    override fun invoke(params: String): Single<Resource<List<Song>>> {
-        return searchRepository.search(params)
+    override fun invoke(params: Query): Single<Resource<List<Song>>> {
+        return when (params) {
+            is Query.Text -> searchRepository.search(params.text)
+            Query.All -> Single.create { it.onSuccess(Resource.Data(listOf())) }
+        }
     }
 }
--- app/src/main/java/com/kamilh/findmysong/interactors/SearchSongs.kt
+++ app/src/main/java/com/kamilh/findmysong/interactors/SearchSongs.kt
@@ -2,7 +2,6 @@ package com.kamilh.findmysong.interactors
 
 import com.kamilh.findmysong.data.Song
 import com.kamilh.findmysong.repository.Resource
-import com.kamilh.findmysong.views.search.Query
 import com.kamilh.findmysong.views.search.SearchParams
 import com.kamilh.findmysong.views.search.Source
 import io.reactivex.Single
@@ -15,17 +14,10 @@ class SearchSongs @Inject constructor(
 ) : SingleInteractor<SearchParams, Resource<List<Song>>> {
 
     override fun invoke(params: SearchParams): Single<Resource<List<Song>>> {
-        return when (params.query) {
-            is Query.Text -> search(params.query, params.source)
-            Query.All -> localSearch.invoke(params.query)
-        }
-    }
-
-    private fun search(query: Query.Text, source: Source): Single<Resource<List<Song>>> {
-        return when (source) {
-            Source.Remote -> remoteSearch.invoke(query.text)
-            Source.Local -> localSearch.invoke(query)
-            Source.All -> Single.zip(localSearch.invoke(query), remoteSearch.invoke(query.text), BiFunction { t1, t2 ->
+        return when (params.source) {
+            Source.Remote -> remoteSearch.invoke(params.query)
+            Source.Local -> localSearch.invoke(params.query)
+            Source.All -> Single.zip(localSearch.invoke(params.query), remoteSearch.invoke(params.query), BiFunction { t1, t2 ->
                 when (t1) {
                     is Resource.Data -> when (t2) {
                         is Resource.Data -> Resource.Data(t2.result.union(t1.result).toList())
